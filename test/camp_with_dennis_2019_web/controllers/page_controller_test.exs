49a21c6f9417230862c51cf8e9a58ab3a8b9c8f1
Dennis Beatty <dennis@dcbeatty.com>
2019-06-04 20:29:05 -0600

    fix test

--- test/camp_with_dennis_2019_web/controllers/page_controller_test.exs
+++ test/camp_with_dennis_2019_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CampWithDennis2019Web.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Americamping 2019"
   end
 end
