7b34072923a5c1b81de3132ea5d8afc7c24488b0
Dennis Beatty <dennis@dcbeatty.com>
2019-06-04 21:12:32 -0600

    use the secret file

--- config/prod.exs
+++ config/prod.exs
@@ -10,5 +10,13 @@ config :logger, level: :info
 
 config :camp_with_dennis_2019, CampWithDennis2019Web.Endpoint, server: true
 
+config :camp_with_dennis_2019, CampWithDennis2019.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "camp_with_dennis_2019_prod",
+  hostname: "localhost",
+  pool_size: 15
+
 # Finally import any environment specific configuration
 import_config "/var/apps/camp_with_dennis_2019/*.exs"
