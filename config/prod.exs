58ba768591971b6309b1602476b5d43253bc9a9a
Dennis Beatty <dennis@dcbeatty.com>
2019-06-04 21:01:57 -0600

    change port

--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,7 @@ use Mix.Config
 
 config :camp_with_dennis_2019, CampWithDennis2019Web.Endpoint,
   url: [host: "campwithdennis.com", port: 443],
+  http: [port: 2267],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
