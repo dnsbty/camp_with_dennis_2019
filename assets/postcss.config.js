9a2fcfd0f2bf0aec9f316aec5788f78669db62c9
Dennis Beatty <dennis@dcbeatty.com>
2019-06-04 20:23:06 -0600

    minify css

--- assets/postcss.config.js
+++ assets/postcss.config.js
@@ -1,3 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    '../lib/camp_with_dennis_2019_web/templates/**/*.html'
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
-  plugins: [require('tailwindcss')('./tailwind.config.js'), require('autoprefixer')],
+  plugins: [
+    require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : [],
+  ],
 }
