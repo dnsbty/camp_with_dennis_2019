64bca9e680613ee50540800f02f818c04c14df91
Dennis Beatty <dennis@dcbeatty.com>
2019-06-10 00:35:12 -0600

    fix release tasks

--- lib/camp_with_dennis_2019/release_tasks.ex
+++ lib/camp_with_dennis_2019/release_tasks.ex
@@ -1,39 +1,22 @@
 defmodule CampWithDennis2019.ReleaseTasks do
-  @start_apps [:ecto, :postgrex]
+  @start_apps [
+    :crypto,
+    :ssl,
+    :postgrex,
+    :ecto,
+    :ecto_sql
+  ]
   @app :camp_with_dennis_2019
-
-  def repos, do: Application.get_env(@app, :ecto_repos, [])
-
-  def seed do
-    prepare()
-    Enum.each(repos(), &run_seeds_for/1)
-    IO.puts("Success!")
-  end
-
-  defp run_seeds_for(repo) do
-    # Run the seed script if it exists
-    seed_script = seeds_path(repo)
-
-    if File.exists?(seed_script) do
-      IO.puts("Running seed script..")
-      Code.eval_file(seed_script)
-    end
-  end
+  @repos Application.get_env(@app, :ecto_repos, [])
 
   def migrate do
-    prepare()
-    Enum.each(repos(), &run_migrations_for/1)
-    IO.puts("Migrations successful!")
-  end
-
-  defp run_migrations_for(repo) do
-    app = Keyword.get(repo.config, :otp_app)
-    IO.puts("Running migrations for #{app}")
-    Ecto.Migrator.run(repo, migrations_path(repo), :up, all: true)
+    start_services()
+    run_migrations()
+    stop_services()
   end
 
   def rollback do
-    prepare()
+    start_services()
 
     get_step =
       IO.gets("Enter the number of steps: ")
@@ -42,44 +25,78 @@ defmodule CampWithDennis2019.ReleaseTasks do
 
     case get_step do
       {int, _trailing} ->
-        Enum.each(repos(), fn repo -> run_rollbacks_for(repo, int) end)
+        Enum.each(@repos, fn repo -> run_rollbacks_for(repo, int) end)
         IO.puts("Rollback successful!")
 
       :error ->
         IO.puts("Invalid integer")
     end
+
+    stop_services()
+  end
+
+  def seed do
+    start_services()
+    run_migrations()
+    run_seeds()
+    stop_services()
+  end
+
+  defp start_services do
+    IO.puts("Starting dependencies...")
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+
+    IO.puts("Starting repos..")
+    Enum.each(@repos, & &1.start_link(pool_size: 2))
+  end
+
+  defp stop_services do
+    IO.puts("Success!")
+    :init.stop()
+  end
+
+  defp run_migrations do
+    Enum.each(@repos, &run_migrations_for/1)
+  end
+
+  defp run_migrations_for(repo) do
+    app = Keyword.get(repo.config(), :otp_app)
+    IO.puts("Running migrations for #{app}")
+    migrations_path = priv_path_for(repo, "migrations")
+    Ecto.Migrator.run(repo, migrations_path, :up, all: true)
+  end
+
+  defp run_seeds do
+    Enum.each(@repos, &run_seeds_for/1)
+  end
+
+  defp run_seeds_for(repo) do
+    seed_script = priv_path_for(repo, "seeds.exs")
+
+    if File.exists?(seed_script) do
+      IO.puts("Running seed script..")
+      Code.eval_file(seed_script)
+    end
+  end
+
+  defp priv_path_for(repo, filename) do
+    app = Keyword.get(repo.config(), :otp_app)
+
+    repo_underscore =
+      repo
+      |> Module.split()
+      |> List.last()
+      |> Macro.underscore()
+
+    priv_dir = "#{:code.priv_dir(app)}"
+
+    Path.join([priv_dir, repo_underscore, filename])
   end
 
   defp run_rollbacks_for(repo, step) do
-    app = Keyword.get(repo.config, :otp_app)
+    app = Keyword.get(repo.config(), :otp_app)
     IO.puts("Running rollbacks for #{app} (STEP=#{step})")
-    Ecto.Migrator.run(repo, migrations_path(repo), :down, all: false, step: step)
+    migrations_path = priv_path_for(repo, "migrations")
+    Ecto.Migrator.run(repo, migrations_path, :down, all: false, step: step)
   end
-
-  defp prepare do
-    IO.puts("Loading #{@app}..")
-    # Load the code for myapp, but don't start it
-    :ok = Application.load(@app)
-
-    IO.puts("Starting dependencies..")
-    # Start apps necessary for executing migrations
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
-
-    # Start the Repo(s) for myapp
-    IO.puts("Starting repos..")
-    Enum.each(repos(), & &1.start_link(pool_size: 1))
-  end
-
-  defp migrations_path(repo), do: priv_path_for(repo, "migrations")
-
-  defp seeds_path(repo), do: priv_path_for(repo, "seeds.exs")
-
-  defp priv_path_for(repo, filename) do
-    app = Keyword.get(repo.config, :otp_app)
-    IO.puts("App: #{app}")
-    repo_underscore = repo |> Module.split() |> List.last() |> Macro.underscore()
-    Path.join([priv_dir(app), repo_underscore, filename])
-  end
-
-  defp priv_dir(app), do: "#{:code.priv_dir(app)}"
 end
